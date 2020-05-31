d4a3eab12b1b637398c6e3f94eeea114c51dd3e4
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-31 15:10:09 +0300

    Change the initial code that was in palindrom_index.rb

--- palindrom_index.rb
+++ palindrom_index.rb
@@ -1,7 +1,16 @@
-=begin
-s = "anna"
-puts s.reverse
-=end
+def palindromeIndex(s)
+    i = 0
+    j = s.size - 1
+    while i < j && s[i] == s[j]
+        i += 1
+        j -= 1
+    end
+    return i if palindrome?(s[0...i]+s[i+1...s.size])
+    return j if palindrome?(s[0...j]+s[j+1...s.size]) 
 
-s = "anna"
-first, second = s.chars.each_slice(s.length / 2).map(&:join)
+    -1
+end
+
+def palindrome?(s)  
+  s.reverse == s
+end
